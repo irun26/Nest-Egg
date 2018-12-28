fcef03c76006c42b934424d1f9e00c4415b710b1
Jimson Vedua <jmsn133@gmail.com>
2018-12-28 13:35:24 -0500

    Labels and textfields created.

--- Nest Egg/ViewController.swift
+++ Nest Egg/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startingBalance: UITextField!
+    @IBOutlet weak var monthlyContribution: UITextField!
+    @IBOutlet weak var yearOfRetirement: UITextField!
+    @IBOutlet weak var interestRate: UITextField!
+    @IBOutlet weak var nestEggAtRetirement: UITextField!
+    
+    @IBAction func calculateButton(_ sender: UIButton) {
+        let startingBalance = Int(self.startingBalance.text!)
+        print("starting balance is \(startingBalance! * 2)")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    
+    
 
 }
 
