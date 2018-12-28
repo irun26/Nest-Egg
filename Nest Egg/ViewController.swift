a6b958d6516f9d0af54139f12c1cb8d39c6daed9
Jimson Vedua <jmsn133@gmail.com>
2018-12-28 12:34:05 -0500

    Initial Commit

---
+++ Nest Egg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nest Egg
+//
+//  Created by Jimson Vedua on 12/28/18.
+//  Copyright © 2018 SameTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
