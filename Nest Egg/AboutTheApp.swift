67c186e79b941b803c9e9c8cb52bfef24d04ae16
Jimson Vedua <jmsn133@gmail.com>
2019-01-01 02:59:38 -0500

    Added AboutTheApp View Controller, segue, and back.

---
+++ Nest Egg/AboutTheApp.swift
@@ -0,0 +1,35 @@
+//
+//  AboutTheApp.swift
+//  Nest Egg
+//
+//  Created by Jimson Vedua on 1/1/19.
+//  Copyright © 2019 SameTeam. All rights reserved.
+//
+
+import UIKit
+
+class AboutTheApp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Nest Egg/ViewController.swift
+++ Nest Egg/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension UIViewController{
+extension UIViewController {
     func HideKeyboard() {
         let Tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DismissKeyboard))
         view.addGestureRecognizer(Tap)
@@ -19,15 +19,12 @@ extension UIViewController{
     }
 }
 
-
-
 class ViewController: UIViewController {
     
     var startingBalance: Double = 0
     var monthlyContribution: Double = 0
     var yearOfRetirement: Double = 0
     var interestRate: Double = 0
-    
 
     @IBOutlet weak var startingBalanceTF: UITextField!
     @IBOutlet weak var monthlyContributionTF: UITextField!
@@ -39,7 +36,6 @@ class ViewController: UIViewController {
         startingBalance = Double(self.startingBalanceTF.text!)!
         monthlyContribution = Double(self.monthlyContributionTF.text!)!
         yearOfRetirement = Double(self.yearOfRetirementTF.text!)!
-//        interestRate = 5.0
         interestRate = Double(self.interestRateTF.text!)!
         
         print("starting balance is \(startingBalance)")
@@ -47,65 +43,33 @@ class ViewController: UIViewController {
         print("year of retirement is \(yearOfRetirement)")
         print("interest rate is \(interestRate)")
 
-//        let doubleInterestRate = Double(interestRate)
         let currentYear: Double = 2018
         let yearsBeforeRetirement: Double =  yearOfRetirement - currentYear
-        
         let nestEgg = balance(P: startingBalance, r: interestRate, c: monthlyContribution, Y: yearsBeforeRetirement)
-//
-        print("nest egg is \(nestEgg)")
-        
-//        let doubleStr = String(format: "%.2f", myDouble) // "3.14"
-
         let stringNestEgg = String(format: "$  %.2f", nestEgg)
-        
-//        nestEggAtRetirementTF.text = String(nestEgg)
-        
-        nestEggAtRetirementTF.text = String(stringNestEgg)
-        
-//        print("float nest egg = \(nestEggAtRetirementTF.text)")
-        
-        
-        
-    }
 
+        nestEggAtRetirementTF.text = String(stringNestEgg)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.HideKeyboard()
     }
-    
-    
-    
     
     func balance(P:Double, r:Double, c:Double, Y:Double) -> Double {
         var bal:Double
     
-//        P = 5000. PMT = 100. r = 5/100 = 0.05 (decimal). n = 12. t = 10.
 //        Total = [ P(1+r/n)^(nt) ] + [ PMT × (((1 + r/n)^(nt) - 1) / (r/n)) ]
-   
         let n: Double = 12
         let rate: Double = r/100
         
         bal = (P * pow(1 + rate / n, n*Y)) + (c * (((pow(1 + rate/n, n*Y)) - 1) / (rate/n)))
-        
-        
-//        bal = pow(P*(1+r/100/12),Y) + c * (pow(1+r/100/12, Y+1) - (1 + r/100/12))/(r/100/12)
-     
-        
         return bal
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        
         self.view.endEditing(true)
-        
         return true
-        
     }
-    
-    
-    
 }
 
