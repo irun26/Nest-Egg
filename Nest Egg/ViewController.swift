486ee1c2132a1b6e88695a4f3411205149fd9c00
Jimson Vedua <jmsn133@gmail.com>
2018-12-31 23:21:46 -0500

    not sure what the issue is

--- Nest Egg/ViewController.swift
+++ Nest Egg/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var startingBalanceAmount: Double = 0
-    var monthlyContributionAmount: Double = 0
+    var startingBalance: Double = 0
+    var monthlyContribution: Double = 0
     var yearOfRetirement: Double = 0
-    var interestRate: Double = 0
+//    var interestRate: Double = 0
     
 
     @IBOutlet weak var startingBalanceTF: UITextField!
@@ -23,22 +23,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var nestEggAtRetirementTF: UITextField!
     
     @IBAction func calculateButton(_ sender: UIButton) {
-        startingBalanceAmount = Double(self.startingBalanceTF.text!)!
-        monthlyContributionAmount = Double(self.monthlyContributionTF.text!)!
+        startingBalance = Double(self.startingBalanceTF.text!)!
+        monthlyContribution = Double(self.monthlyContributionTF.text!)!
         yearOfRetirement = Double(self.yearOfRetirementTF.text!)!
-        interestRate = Double(self.interestRateTF.text!)!
+//        interestRate = 5.0
+//        interestRate = Float(self.interestRateTF.text!)!
         
-        print("starting balance is \(startingBalanceAmount)")
-        print("monthly contribution is \(monthlyContributionAmount)")
-        print("year of retirement is \(yearOfRetirement)")
-        print("interest rate is \(interestRate)")
+//        print("starting balance is \(startingBalanceAmount)")
+//        print("monthly contribution is \(monthlyContributionAmount)")
+//        print("year of retirement is \(yearOfRetirement)")
+//        print("interest rate is \(interestRate)")
+//
+//        let doubleInterestRate = Double(interestRate)
         
+        let nestEgg = balance(P: startingBalance, r: 5.0, c: monthlyContribution, Y: yearOfRetirement)
         
+        nestEggAtRetirementTF.text = String(nestEgg)
         
-        
-        nestEggAtRetirementTF.text = String(balance(P: startingBalanceAmount, r: interestRate, c: monthlyContributionAmount, Y: yearOfRetirement))
-        
-        print("float nest egg = \(nestEggAtRetirementTF.text)")
+//        print("float nest egg = \(nestEggAtRetirementTF.text)")
         
         
         
